f3aefc971f8e3262b0f319acca32969437451022
Annam, Kishore <C-Kishore.Annam@charter.com>
2017-04-28 16:57:05 +0530

    nav bar with menu button

--- src/ct/case-home/case-home.component.ts
+++ src/ct/case-home/case-home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaseHomeComponent implements OnInit {
 
+  isCollapsed = true;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleNavButton() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
